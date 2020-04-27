f6759a84a400a104fd40275290a0265c248744dd
JadeLiber <e.zalesskaya35@gmail.com>
2020-04-28 02:07:24 +0300

    some changes

--- lab2/test.py
+++ lab2/test.py
@@ -2,7 +2,7 @@ import spyre.server
 import datetime
 import os
 import pandas as pd
-
+import matplotlib.pyplot as plt 
 
 class VHIApp(spyre.server.App):
     title = "VHI analysis"
@@ -35,42 +35,32 @@ class VHIApp(spyre.server.App):
                             {"label":"Volyn",            "value": 2},
                             {"label":"Zaporizhzhya",     "value": 7},
                             {"label":"Zhytomyr",         "value": 5}],
-                "key": 'province',
-                "linked_key": 'title',
-                "linked_type": 'text',
-                "linked_value":"choose",
-                "action_id": "update_data"},
+                "key": 'province'},
     
                 {"type":'dropdown',
                  "label": 'Index',
                  "options" : [{"label":"VHI", "value":"VHI"},
                               {"label":"TCI","value":"TCI"},
                               {"label":"VCI","value":"VCI"}],
-                 "key": 'index',
-                 "linked_key": 'title',
-                 "linked_type": 'text',
-                 "linked_value":"choose",
-                 "action_id": "update_data"
+                 "key": 'index'
                  },
                  
                 {"type": 'dropdown',
                   "label": "Year",
                   "options": [{'label': str(i), 'value': i} for i in range(1982, int(datetime.datetime.now().strftime("%Y"))+1)],
                   'key': 'year',
-                  'value': '1982',
-                  "action_id": "update_data"},
+                  'value': '1982'},
                   
                 {"type": 'text',
                   "label": "first week",
                   'key': 'first_week',
-                  'value': '1',
-                  "action_id": "update_data"},
+                  'value': '1'},
                    
                 {"type": 'text',
                   "label": "last week",
                   'key': 'last_week',
-                  'value': '52',
-                  "action_id": "update_data"}]
+                  'value': '52'
+                  }]
 
     controls = [
         {
@@ -91,30 +81,41 @@ class VHIApp(spyre.server.App):
         },
         {
             "type": "table",
-            "id": "table_id",
+            "id": "mean_table",
             "control_id": "update_data",
             "tab": "Table"
-        }]
+        }
+        ]
+    def __init__(self):
+        df = pd.DataFrame(columns=['year','week', 'SMN','SMT','VCI','TCI','VHI','province'])
+        colnames=['year','week', 'SMN','SMT','VCI','TCI','VHI','province']
+        for i in range (1, 28):
+            file_name = f"data\_id_{i}.csv"
+            temp = pd.read_csv(file_name, sep=",", names = colnames, index_col=False, header=None)
+            temp['province']=i
+            temp = temp[temp.VHI!=-1]
+            df = df.append(temp, ignore_index=True)
+        self.df = df
     def getData(self, params):      
         province = params['province']
         year = params['year']
         first_week = int(params['first_week'])
         last_week =int( params['last_week'])
         index = params ['index']
-        file_name = f"data\_id_{province}.csv"
-        colnames=['year','week', 'SMN','SMT','VCI','TCI','VHI','unnamed'] #задаём имена столбиков
-        df = pd.read_csv(file_name, sep=",", names = colnames, usecols =['year','week', 'SMN','SMT','VCI','TCI','VHI'], index_col=False, header=None) #чтение файла в формате csv (excel)
+        df = self.df
         df['year'] = df['year'].astype(str)
-        df = df[df.year==year]
-       
-        #df1['year'] = df1['year'].astype(str)
-        #df = df[(df.week>=first_week)& (df.week<=last_week)][['year', 'week', index]]     
+        df['province'] = df['province'].astype(str)
+        df = df[(df.year==year)&(df.province == province)]
+        df['week'] = df['week'].astype(int)
+        df = df[(df.week>=first_week)& (df.week<=last_week)]   
         return df[['year','week',index]]
      
     def getPlot(self, params):
-        
-        df = self.getData(params)
-        return df.set_index(df['week']).plot()
+        index = params['index']
+        df = self.getData(params) 
+        df1 = df[[index]]
+        return df1.set_index(df['week']).plot()
+
     
 
 app = VHIApp()
